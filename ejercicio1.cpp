446b13b23af66ed503fde6f07723db9cb0112c6b
nicolasrubin985 <60698011+nicolasrubin985@users.noreply.github.com>
2025-04-04 20:10:11 -0300

    Add files via upload

---
+++ ejercicio1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main (){
+	
+	long edad;
+	string nombre;
+	string apellido;
+	float otroNum;
+	cout<< "Escribe tu edad:"<< endl;
+	cin>>edad;
+	cout<<"\n tu edad es:  "<< edad <<endl;
+	cout << "escriba el nombre"<< endl;
+	cin>>nombre;
+	cout << "escriba el apellido"<< endl;
+	cin>>apellido;
+	cout<<"\n el nombre es: "<< nombre<< endl;
+	cout<<"\n el apellido es: "<< apellido<< endl;
+	cout<< "Digite un numero de tipo flotante "<<endl;
+	cin>> otroNum;
+	cout<<"\ El numero que digito es "<< otroNum;
+	
+	return 0;
+}
---
+++ ejercicio2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main (){
+	double cociente;
+	int a,b; 
+	cout<<"ingrese dos numeros enteros: "<<endl;
+	cin>>a>>b;
+	cociente =(double) a/b;
+	cout<<"la division es: "<< cociente<<endl;
+		
+	return 0;
+}
