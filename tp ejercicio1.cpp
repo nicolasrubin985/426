541aac1e44370e3aeeacd4fe83669658ca632ba2
nicolasrubin985 <60698011+nicolasrubin985@users.noreply.github.com>
2025-04-11 18:14:26 -0300

    Add files via upload

---
+++ tp ejercicio1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int base ,altura, perimetro , area ;
+	cout<< "ingrese la base: "<< endl;
+	cin>> base ;
+	cout<< "ingrese la altura: "<< endl;
+	cin>> altura;
+	
+	perimetro = 2*(altura * base) ;
+	
+	area = altura * base ;
+	
+	cout<< " el area es: "<< area<<endl<< "el perimetro es: "<< perimetro<< endl;
+	
+	
+	
+	
+	
+	
+}
