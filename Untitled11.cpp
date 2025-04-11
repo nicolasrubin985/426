16c73fda45868c771a552b96173b5d88f9b80906
nicolasrubin985 <60698011+nicolasrubin985@users.noreply.github.com>
2025-04-11 20:28:13 -0300

    Add files via upload

---
+++ Untitled11.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main ()
+{
+	int i=1;
+	
+	do
+	{
+		
+		cout<<"soy la variable i, mi valor en esta interaccion es: "<<i << endl;
+		i++;
+		
+	}while (i<11);
+	
+}
